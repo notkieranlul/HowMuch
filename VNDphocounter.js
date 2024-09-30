9bc48963b9319a1fe837b66f4fd6f05cbe3f1844
kieranlul <kierandwallace@outlook.com>
2024-09-30 15:04:05 +0100

    pho counter 2
    made changes to the pho counter method to add an image rather than text if the value of 10 or greater is entered.

--- VNDphocounter.js
+++ VNDphocounter.js
@@ -7,13 +7,22 @@ inputField.addEventListener("input", checkInputValue);
 function checkInputValue() {
     // Get the current value of the input field
     const inputValue = inputField.value;
+    const phoImage = document.getElementById("pho-counter");
+
+    phoImage.innerHTML = '';
 
     // Check if the value is greater than 10
     if (inputValue >= 10) {
         // Update the text if the value is greater than 10
-        document.getElementById("target-text").textContent = "this is working";
+        const img = document.createElement("img");
+        img.src = "https://t4.ftcdn.net/jpg/05/45/47/21/360_F_545472143_yPWPWqfbvvySoRfmzH6GJmCkQDPgMNHc.jpg";
+        img. width = 50;
+    phoImage.appendChild(img);
     } else {
         // Update the text if the value is 10 or less
-        document.getElementById("target-text").textContent = "";
+        const img = document.createElement("img");
+        img.src = "";
+        img. width = 10;
+        phoImage.appendChild(img);
     }
 }
